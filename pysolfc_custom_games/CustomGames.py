0838f4f3d69e9ad34d1837fe9ca486e3f2b650e9
JorgeT17 <jorget17@gmail.com>
2022-08-15 23:58:28 +0200

    custom games in one file

---
+++ pysolfc_custom_games/CustomGames.py
@@ -0,0 +1,54 @@
+## -*- coding: utf-8 -*-
+## THIS FILE WAS GENERATED AUTOMATICALLY BY THE SOLITAIRE WIZARD
+## DO NOT EDIT
+
+from pysollib.customgame import CustomGame, registerCustomGame
+
+class MyCustomGame(CustomGame):
+    WIZARD_VERSION = 1
+    SETTINGS = {
+        'preset': 'FreeCell',
+        'name': 'SixOff',
+        'skill_level': 'Mostly skill',
+        'rows_type': 'Same suit',
+        'rows_base_card': 'King',
+        'rows_max_move': 'Top card',
+        'rows_super_move': 1,
+        'reserves_num': 6,
+        'deal_face_up': 6,
+        'gameid': 200001,
+            }
+
+registerCustomGame(MyCustomGame)
+
+class MyCustomGame(CustomGame):
+    WIZARD_VERSION = 1
+    SETTINGS = {
+        'preset': 'FreeCell',
+        'name': 'FiveOff',
+        'skill_level': 'Mostly skill',
+        'rows_type': 'Same suit',
+        'rows_base_card': 'King',
+        'rows_max_move': 'Top card',
+        'rows_super_move': 1,
+        'reserves_num': 5,
+        'deal_face_up': 5,
+        'gameid': 200002,
+            }
+
+registerCustomGame(MyCustomGame)
+
+class MyCustomGame(CustomGame):
+    WIZARD_VERSION = 1
+    SETTINGS = {
+        'preset': 'FreeCell',
+        'name': 'Free Seven',
+        'skill_level': 'Mostly skill',
+        'rows_num': 7,
+        'rows_max_move': 'Top card',
+        'rows_super_move': 1,
+        'deal_face_up': 6,
+        'gameid': 200003,
+            }
+
+registerCustomGame(MyCustomGame)
