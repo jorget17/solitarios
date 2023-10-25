3c8dc753d72277b12a875d1637259f2a1d9b2e14
JorgeT17 <jorget17@gmail.com>
2023-10-25 08:54:22 +0200

    autopush

--- sudokus/hodoku/analyzer/just_sudokus_analyze.sh
+++ sudokus/hodoku/analyzer/just_sudokus_analyze.sh
@@ -1,5 +1,5 @@
 python3 zeroes.py
-java -jar hodoku.jar /vp /vst /bs hg.txt /o hodoku_generated_out.txt
+java -jar hodoku.jar /vp /vst /bs sudokus.txt /o hodoku_generated_out.txt
 python3 just_sudokus_export.py
 rm hodoku_generated_out.txt
 rm hg_one_line.txt
